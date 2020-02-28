7c93cf6a6079991681308f9ac4516d5d0e74f03e
Julien Dusser <julien.dusser@free.fr>
2020-02-28 16:00:15 +0100

    Fix create-CA.sh variables
    Signed-off-by: Julien Dusser <julien.dusser@free.fr>

--- create-CA.sh
+++ create-CA.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CA_VALIDITY = 3650
-CA_KEY_SIZE = 4096
+CA_VALIDITY=3650
+CA_KEY_SIZE=4096
 
 mkdir -p "CA/private" "CA/newcerts"
 chmod 700 "CA/private"
@@ -9,5 +9,5 @@ touch "CA/index.txt"
 touch "CA/index.txt.attr"
 echo 01 > "CA/serial"
 
-openssl genrsa -des3 -out "CA/private/cakey.pem" $CA_KEY_SIZE
-openssl req -new -x509 -days $CA_VALIDITY -key "CA/private/cakey.pem" -out "CA/cacert.pem"
+openssl genrsa -des3 -out "CA/private/cakey.pem" ${CA_KEY_SIZE}
+openssl req -new -x509 -days ${CA_VALIDITY} -key "CA/private/cakey.pem" -out "CA/cacert.pem"
